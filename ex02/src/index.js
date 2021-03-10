e5abb08aeef9e672f836477be4a87fc09b78bb02
alko8910 <almedinemkic@gmail.com>
2021-03-10 19:03:32 +0100

    Dodao Day03

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+
+    // Ovako se pise komentar u jednom redu
+
+    /*
+    Ovako se pise 
+    komentar u vise redova
+    */
+    // Only change code above this line
+    }
+    console.log(main());
+    module.exports = main;
---
+++ ex05/src/index.js
@@ -0,0 +1,7 @@
+function main() {
+    // Only change code below this line
+    var x = 8;
+    // Only change code above this line
+    }
+    console.log(main());
+    module.exports = main;
